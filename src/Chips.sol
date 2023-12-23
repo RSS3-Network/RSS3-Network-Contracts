d92fbd72b754d820859485d47e6fa65734477ef9
Albert <albert@rss3.io>
2023-12-23 12:05:26 +0800

    feat: use ERC721AUpgradeable

--- src/Chips.sol
+++ src/Chips.sol
@@ -7,8 +7,9 @@ import {
     ERC721Consecutive
 } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Consecutive.sol";
 import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import "erc721a-upgradeable/contracts/ERC721AUpgradeable.sol";
 
-contract Chips is Initializable, ERC721Consecutive {
+contract Chips is Initializable, ERC721AUpgradeable {
     address internal _staking;
 
     modifier onlyStaking() {
@@ -16,20 +17,22 @@ contract Chips is Initializable, ERC721Consecutive {
         _;
     }
 
-    constructor() ERC721("RSS3 Chips", "Chips") {
+    constructor() {
         _disableInitializers();
     }
 
-    function initialize(address staking_) external initializer {
-        _staking = staking_;
+    function initialize(
+        address staking,
+        string calldata name,
+        string calldata symbol
+    ) external initializerERC721A initializer {
+        _staking = staking;
+
+        __ERC721A_init(name, symbol);
     }
 
-    function mint(address account, uint256 id) public onlyStaking {
-        _mint(account, id);
-    }
-
-    function mintBatch(address to, uint96 batchSize) public onlyStaking returns (uint256) {
-        return _mintConsecutive(to, batchSize);
+    function mintBatch(address to, uint256 quantity) public onlyStaking {
+        _mint(to, quantity);
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
