645729012e046fd41cd224a88bd24f3c9e0ca653
Albert <albert@rss3.io>
2024-05-20 22:11:12 +0800

    fix: typo

--- src/interfaces/IErrors.sol
+++ src/interfaces/IErrors.sol
@@ -78,7 +78,7 @@ interface IErrors {
     /// @dev Tax rate is not zero for public good node.
     error PublicGoodNodeTaxNotZero();
 
-    /// @dev Cant set tax for public good node
+    /// @dev Can't set tax for public good node
     error NodeIsPublicGood();
 
     /// @dev Batch size is zero.
