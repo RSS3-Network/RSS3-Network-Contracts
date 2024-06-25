672232941f684dac44bc02a55eda3455ca5dcfdf
Albert <albert@rss3.io>
2024-06-25 21:10:42 +0800

    feat: add NetworkParams contract

---
+++ src/NetworkParams.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+// solhint-disable comprehensive-interface
+pragma solidity 0.8.20;
+
+import {AccessControlEnumerable} from "@openzeppelin/contracts/access/extensions/AccessControlEnumerable.sol";
+import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
+contract NetworkParams is Initializable, AccessControlEnumerable {
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    string internal _params;
+
+    event ParamsSet(string params);
+
+    /**
+     * @notice Initializes the NetworkParams contract.
+     * @param adminAccount Address who can set the NetworkParams contract.
+     */
+    function initialize(address adminAccount) external initializer {
+        _grantRole(ADMIN_ROLE, adminAccount);
+        _grantRole(0x00, adminAccount);
+    }
+
+    /**
+     * @notice Sets params for the contract.
+     * @param params The params to set.
+     */
+    function setParams(string calldata params) external onlyRole(ADMIN_ROLE) {
+        _params = params;
+
+        emit ParamsSet(params);
+    }
+
+    /**
+     * @notice Gets params from the contract.
+     * @return params The params to get.
+     */
+    function getParams() external view returns (string memory) {
+        return _params;
+    }
+}
