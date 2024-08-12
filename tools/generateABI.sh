ee0be60ce2b4365f5fec9645fe236911840520f4
Albert <albert@rss3.io>
2024-08-12 17:53:18 +0800

    feat: add script for generating abi
    Signed-off-by: Albert <albert@rss3.io>

--- tools/generateABI.sh
+++ tools/generateABI.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -x
 
+ABI_DIR=./deployments/abi/
+
 forge build --silent
-jq '.abi' ./out/Events.sol/Events.json > deployments/abi/Events.abi
-jq '.abi' ./out/Chips.sol/Chips.json > deployments/abi/Chips.abi
-jq '.abi' ./out/Staking.sol/Staking.json > deployments/abi/Staking.abi
-jq '.abi' ./out/Settlement.sol/Settlement.json > deployments/abi/Settlement.abi
 
-
+for contract in Staking Chips Settlement Events NetworkParams
+do
+  # extract abi and bin files
+  forge inspect ${contract} abi > ${ABI_DIR}/${contract}.abi
+done
