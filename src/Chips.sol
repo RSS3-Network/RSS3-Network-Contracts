7ebc3e1c508350558fa5e3c1c56001d03d0ce418
Albert <albert@rss3.io>
2024-09-02 19:56:59 +0800

    feat: update chips version 2.0.1
    Signed-off-by: Albert <albert@rss3.io>

--- src/Chips.sol
+++ src/Chips.sol
@@ -16,7 +16,7 @@ import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 contract Chips is IChips, Initializable, ERC721 {
     using Strings for uint256;
 
-    string public constant version = "2.0.0";
+    string public constant version = "2.0.1";
 
     /// @dev Staking contract address.
     address internal _staking;
